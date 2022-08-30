6747f799bfefc52433dfb111a9ab68f3026a001f
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-30 20:09:38 -0300

    inicio exemplos metaprogramação

---
+++ metaprogramacao/1-exec.rb
@@ -0,0 +1,10 @@
+a = "sddss TTAHAb sdSKSHDJDK  dssdsS SSSSSdsdds "
+
+def a.minusculo
+  self.downcase
+end
+
+puts "a = #{a.minusculo}"
+
+b = "DANILO"
+puts "a = #{b.minusculo}"
---
+++ metaprogramacao/2-exec.rb
@@ -0,0 +1,12 @@
+String.class_eval do
+  def minusculo
+    self.downcase
+  end
+end
+
+a = "sddss TTAHAb sdSKSHDJDK  dssdsS SSSSSdsdds "
+puts "a = #{a.minusculo}"
+
+
+b = "DANILO"
+puts "a = #{b.minusculo}"
